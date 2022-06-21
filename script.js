89d9353296f9f6cbaca5255f0a48091154793c5b
Hayden <asianhaydenxd@gmail.com>
2022-06-20 23:44:32 -0400

    Create game constructor

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const width = 7;
 const height = 6;
 
-function generateBoard(width, height) {
+const Slot = {
+    Empty: "Empty",
+    Player1: "Player1",
+    Player2: "Player2",
+}
+
+function Game() {
+    this.board = new Array(height).fill(null).map( () => new Array(width).fill(Slot.Empty) );
+    this.put = function(slot, x, y) {
+        this.board[y][x] = slot;
+    }
+}
+
+function generateBoard(game, width, height) {
     const board = document.getElementById("board");
 
     while (board.firstChild) {
@@ -15,6 +28,7 @@ function generateBoard(width, height) {
         for (let cellNumber = 0; cellNumber < width; cellNumber++) {
             const cell = document.createElement("div");
             cell.className = 'cell';
+            cell.slot = game.board[rowNumber][cellNumber]
 
             row.appendChild(cell);
         }
@@ -23,4 +37,10 @@ function generateBoard(width, height) {
     }
 }
 
-generateBoard(width, height);
+const game = new Game();
+
+game.put(Slot.Player1, 0, 5); // Place a red piece at coords (0, 5)
+console.table(game.board);
+
+
+generateBoard(game, width, height);
